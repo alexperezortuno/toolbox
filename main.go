919212814f89718ff25fd1e5528241808e9e044f
alexperezortuno <alexander.perez@sixbell.com>
2024-01-24 16:04:02 -0300

    change build

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/alexperezortuno/toolbox/cmd"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
+var version = "0.0.1"
+
 func main() {
+	fmt.Printf("Toolbox: v%s\n", version)
 	ctx := make(chan os.Signal)
 	signal.Notify(ctx, os.Interrupt, syscall.SIGTERM)
 
