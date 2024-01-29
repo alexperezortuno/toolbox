609e10e038f926eda4f6f76bbe295bbd3ce5b6a9
alexperezortuno <alexander.perez@sixbell.com>
2024-01-29 13:09:53 -0300

    change help

--- cmd/file/download.go
+++ cmd/file/download.go
@@ -21,13 +21,8 @@ var (
 // downloadCmd represents the download command
 var downloadCmd = &cobra.Command{
 	Use:   "download",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Download a file(s) from a URL",
+	Long:  `For more information about this command, please visit: https://www.gnu.org/software/wget/manual/wget.html`,
 	Run: func(cmd *cobra.Command, args []string) {
 		var c *exec.Cmd
 		var s []string
